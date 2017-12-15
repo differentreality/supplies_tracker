80132874563421b6f00c50f0973ab20efaf4e7f3
Stella Rouzi <differentreality@gmail.com>
2017-12-15 22:15:23 +0200

    Introduce bootstrap

--- models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.utils import timezone
-
-class Item(models.Model):
-    name = models.CharField(max_length=200)
-    description = models.TextField()
-    # price_bought = models.FloatField()
-    # reimbursement = models.FloatField()
-    created_date = models.DateTimeField(default=timezone.now)
