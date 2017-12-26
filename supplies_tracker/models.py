416095b00f3122f6a2c659723343f99f7d374f60
Stella Rouzi <differentreality@gmail.com>
2017-12-26 12:10:11 +0200

    [wip] Creating models

--- supplies_tracker/models.py
+++ supplies_tracker/models.py
@@ -3,8 +3,23 @@ from django.db import models
 from django.utils import timezone
 
 class Item(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, null=False, blank=False)
     description = models.TextField()
     price_bought = models.FloatField(default=0)
     reimbursement = models.FloatField(default=0)
-    created_date = models.DateTimeField(default=timezone.now)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Space(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False)
+    description = models.TextField
+    address = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Storage(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False)
+    capacity = models.FloatField
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    space_id = models.ForeignKey(Space, on_delete=models.CASCADE)
