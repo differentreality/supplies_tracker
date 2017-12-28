35c370a9d88a9a6039215089aef4dc607594d0b9
Antonis <antonisagg@outlook.com>
2017-12-28 15:56:01 +0200

    many to many

--- supplies_tracker/models.py
+++ supplies_tracker/models.py
@@ -2,6 +2,7 @@ from hamlpy.views.generic import HamlExtensionTemplateView
 from django.db import models
 from django.utils import timezone
 
+
 class Item(models.Model):
     name = models.CharField(max_length=200, null=False, blank=False)
     description = models.TextField()
@@ -9,6 +10,8 @@ class Item(models.Model):
     reimbursement = models.FloatField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    items = models.ManyToManyField('Storage',through='Items_Storage',related_name='item')
+
 
 class Space(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
@@ -23,3 +26,12 @@ class Storage(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     space_id = models.ForeignKey(Space, on_delete=models.CASCADE)
+
+class Items_Storage(models.Model) :
+    item = models.ForeignKey(Item,related_name= 'content')
+    storage = models.ForeignKey(Storage, related_name='content')
+    date_added = models.DateTimeField(auto_now_add=True)
+    quantity = models.IntegerField(null=False,blank=False)
+    price_bought = models.FloatField
+    price_donated = models.FloatField
+
