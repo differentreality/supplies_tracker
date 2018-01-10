fac1d776e6228c091a483c66c7e5ebb2f4f3fd33
Stella Rouzi <differentreality@gmail.com>
2018-01-10 17:19:49 +0200

    Use auth_login for auto login after signup

--- supplies_tracker/views.py
+++ supplies_tracker/views.py
@@ -120,7 +120,7 @@ def signup(request):
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
-            login(request, user)
+            auth_login(request, user)
             return redirect('home.html.haml')
     else:
         form = SignUpForm()
