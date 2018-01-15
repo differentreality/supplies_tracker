f2f4ee5d98bdb96fddcbf8932be44bfc8ef04596
Stella Rouzi <differentreality@gmail.com>
2018-01-15 10:57:54 +0200

    Select space_id in storages#new

--- supplies_tracker/forms.py
+++ supplies_tracker/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
+from .models import Space
 
 class item_form(forms.Form):
     name = forms.CharField(label='Name', max_length=100, widget=forms.TextInput(attrs={'autofocus': 'autofocus'}))
@@ -10,8 +11,15 @@ class item_form(forms.Form):
     image = forms.ImageField(label='Add a photo of Item', required=False)
 
 class storage_form(forms.Form):
-    name = forms.CharField(label='Name', max_length=100, widget=forms.TextInput(attrs={'autofocus': 'autofocus'}))
-    image = forms.ImageField(label='Add a photo of Storage', required=False)
+  name = forms.CharField(label='Name', max_length=100, widget=forms.TextInput(attrs={'autofocus': 'autofocus'}))
+  image = forms.ImageField(label='Photo', required=False)
+
+  def __init__(self, user, *args, **kwargs):
+    super(storage_form, self).__init__(*args, **kwargs)
+    if user is not None and user.id is not None:
+        self.fields['space_id'] = forms.ModelChoiceField(label='Space',
+                                                        queryset=Space.objects.filter(user_id=user.id),
+                                                        empty_label= '(Please select)')
 
 class space_form(forms.Form):
     name = forms.CharField(label='Name', max_length=100, widget=forms.TextInput(attrs={'autofocus': 'autofocus'}))
