220632edd103c1db2758e6b30696623482782fa8
Stella Rouzi <differentreality@gmail.com>
2018-01-17 11:24:25 +0200

    Style changes

--- supplies_tracker/models.py
+++ supplies_tracker/models.py
@@ -15,7 +15,7 @@ class Item(models.Model):
     name = models.CharField(max_length=200, null=False, blank=False)
     description = models.TextField()
     price_bought = models.FloatField(default=0)
-    reimbursement = models.FloatField(default=0)
+    reimbursement = models.FloatField(default=0, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     storages = models.ManyToManyField('Storage', through='Items_Storage', related_name='Storage')
