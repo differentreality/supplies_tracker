ada3c799c0b0dc19acd4ffa2c99b70765aaf4003
Antonis <antonisagg@outlook.com>
2018-02-08 18:51:59 +0200

    secret key fixed

--- supplies_tracker/settings.py
+++ supplies_tracker/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'oz*^bg-_i9r=**-me%s(@7n)^5+zonhbta=-4osh5eqgj_&mwt'
+SECRET_KEY = 'SECRET'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
